f7e1d4d7f808e55cc5a8f04b950ee504de564dbf
Manho Joung <MamboFamily2024@gmail.com>
2024-11-04 20:32:42 -0800

    initial template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def credits(request):
     content = "Manho Joung"
     return HttpResponse(content, content_type="text/plain")
+
+def news(request):
+    data = {
+        'news': [
+            "MamboStory has a news page!",
+            "MamboStory has its first web page",
+        ],
+    }
+    return render(request, "news2.html", data)
